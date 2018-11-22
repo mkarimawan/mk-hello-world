76ce777a3cd884c4843d3ed7bce847bc10f62f6f
mkarimawan <mkarimawan@gmail.com>
2018-11-22 16:46:14 +1100

    chanegs to the main .py file

--- rand_win.py
+++ rand_win.py
@@ -3,7 +3,7 @@ import random
 
 num = random.randint(1,10)
 
-guess = int(input("Guess a number b/w 1 and 10:"))
+guess = int(input("Guess a number between 1 and 10:"))
 
 while guess!=num:
     guess = int(input('Guess again:'))
